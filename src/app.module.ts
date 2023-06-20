fd818de6b0da32a97ac3fce9fc0a8f0a701c2a16
Marcel Sena Rios <carlosmarcel@brq.com>
2023-06-20 10:52:55 -0300

    create crud veiculos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmpresaModule } from './empresa/empresa.module';
+import { VeiculosModule } from './veiculos/veiculos.module';
 
 @Module({
-  imports: [EmpresaModule],
+  imports: [EmpresaModule, VeiculosModule],
   controllers: [AppController],
   providers: [AppService],
 })
